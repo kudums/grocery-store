ba7837e19de1bacaa8d55feadc90470c42bc9da1
Sushma Kudum <sushmakudum@macbook-pro.kpn>
2024-11-19 13:15:49 +0100

    cros config

---
+++ src/main/java/com/payaut/grocery_store/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.payaut.grocery_store.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // Allow all paths
+                .allowedOrigins("http://localhost:8080", "https://grocery-store-production-d971.up.railway.app") // Add the Swagger UI URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allow necessary HTTP methods
+                .allowedHeaders("*"); // Allow all headers
+    }
+}
+
